cf0b3491a25389038c85266e88fcda87f4e7b0eb
Hugo Miranda <hsaraiva08@gmail.com>
2020-06-18 14:37:30 +0200

    Updates

---
+++ precache-manifest.2d9a797a68c5083b30d455f942ce8722.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a71056076be37a09169dcac9ca9c088d",
+    "url": "/Portfolio-Website/index.html"
+  },
+  {
+    "revision": "2ce59b2e3b2689a80bdb",
+    "url": "/Portfolio-Website/static/css/main.9f632966.chunk.css"
+  },
+  {
+    "revision": "e3a19036c6582b42ad43",
+    "url": "/Portfolio-Website/static/js/2.c155cdd7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio-Website/static/js/2.c155cdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ce59b2e3b2689a80bdb",
+    "url": "/Portfolio-Website/static/js/main.eed0c870.chunk.js"
+  },
+  {
+    "revision": "81dfee1bcd1cfd101105",
+    "url": "/Portfolio-Website/static/js/runtime-main.8928ca10.js"
+  },
+  {
+    "revision": "ae7f22804f0d1097ce18b1fdccd1e8d1",
+    "url": "/Portfolio-Website/static/media/moticon_3d_shoe.ae7f2280.png"
+  },
+  {
+    "revision": "2fd7b23cc141c830d6353af31d0450c5",
+    "url": "/Portfolio-Website/static/media/mugshot.2fd7b23c.png"
+  },
+  {
+    "revision": "a83484d012d7a23e93acad2bc7d29426",
+    "url": "/Portfolio-Website/static/media/sardinha.a83484d0.png"
+  }
+]);
