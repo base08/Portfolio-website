62ce31445182567fe387517c2d7e877024e39e9c
Hugo Miranda <hsaraiva08@gmail.com>
2020-09-30 11:48:22 +0200

    alpha version

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -2,6 +2,7 @@ import React from "react";
 import mugshot from "../mugshot.png";
 import "./Greeting.css";
 import Footer from "./Footer";
+import ReactGA from "react-ga";
 
 function Greeting() {
   return (
@@ -12,12 +13,20 @@ function Greeting() {
         I’m a UX/UI Designer, that also likes to play with code and computers.
       </h4>
       <div className="Buttons-CTA">
-        <a
+        {/*    <a
           className="Button Portfolio"
           href="https://drive.google.com/drive/folders/1zXJF_bYAIebHWZK-FpKLJBfO5YFIBK1s?usp=sharing"
         >
           FULL PORTFOLIO
-        </a>
+        </a> */}
+        <ReactGA.OutboundLink
+          className="Button Portfolio"
+          eventLabel="GDRIVE PORTFOLIO"
+          to="https://drive.google.com/drive/folders/1zXJF_bYAIebHWZK-FpKLJBfO5YFIBK1s?usp=sharing"
+          target="_blank"
+        >
+          FULL PORTFOLIO
+        </ReactGA.OutboundLink>
         <a className="Button Contact" href="mailto:hsaraiva08@gmail.com">
           CONTACT ME
         </a>
